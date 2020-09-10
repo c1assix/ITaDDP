96d136b6a5d7c2c1cae0644424dadfcebc4bdb45
Lev Kupchinov <lev.c1assix@gmail.com>
2020-09-10 18:12:42 +0300

    added live search for cocktails on main page

--- public/js/views/pages/Main.js
+++ public/js/views/pages/Main.js
@@ -7,7 +7,7 @@ let Main = {
     render: async () => {
         let view = `
         <div class="search-block">
-            <input type="search" class="search-input">
+            <input id="live-search" type="search" class="search-input">
             <a href="/add" id="add-cocktail-button" class="add-cocktail-button">Add</a>
         </div>
         <ul id="cocktail-table" class="table-wrap"></ul>`
@@ -22,6 +22,7 @@ let Main = {
             for (const element of cocktails) {
                 let item = document.createElement('li');
                 item.id = window.btoa(element.id);
+                item.dataset.name =  element.name;
                 let card = await CocktailCard.render(element);
                 item.innerHTML = `${card}`;
                 item.addEventListener('click', (event) =>{
@@ -31,6 +32,24 @@ let Main = {
                 list.appendChild(item);
             }
         }
+
+        document.getElementById("live-search").addEventListener("keyup", (event)=>{
+            let input, filter, ul, li, a, i, txtValue;
+            input = document.getElementById('live-search');
+            filter = input.value.toUpperCase();
+            ul = document.getElementById("cocktail-table");
+            li = ul.getElementsByTagName('li');
+
+            for (i = 0; i < li.length; i++) {
+                txtValue = li[i].dataset.name;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+                }
+            }
+
+        })
     }
 }
 
